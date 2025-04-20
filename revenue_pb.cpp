24be0787d2c6cf227c651f5a40eafb5b7de01636
illyaGavlovskyi <151693791+illyaGavlovskyi@users.noreply.github.com>
2025-04-19 17:53:32 -0700

    Create revenue_pb.cpp

---
+++ revenue_pb.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+    int n = 4;
+    int r[n];
+    r = {1000, 100, 1000, 1000};
+    int c[n];
+    c = {500, 5000, 500, 100};
+    
+    int day[n + 1];
+    day[0] = 0;
+    day[1] = max(r[0], c[0]);
+    for(int i = 2; i < n + 1; i++)
+    {
+        day[i] = max(day[i - 1] + r[i], day[i - 2] + c[i]);
+    }
+    ofstream out("revenue_pb_out.txt");
+    for (int i = 0; i <= n; i++) {
+        out << "Day " << i << ": " << dp[i] << "\n";
+    }
+    out << "\nOptimal Total Revenue: " << dp[n] << "\n";
+    out.close();
+    return 0;
+}
